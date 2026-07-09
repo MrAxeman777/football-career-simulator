5d8b5abad1bdbebb4e704a441e96e6b808a0ef5d
MrAxeman777 <arsharma892@gmail.com>
2026-07-09 12:04:27 -0500

    Create player.py

---
+++ player.py
@@ -0,0 +1,10 @@
+class Player:
+    def __init__(self, name, position, age, nationality, club, rating, potential):
+        self.name = name
+        self.position = position
+        self.age = age
+        self.nationality = nationality
+        self.club = club
+
+        self.rating = rating
+        self.potential = potential

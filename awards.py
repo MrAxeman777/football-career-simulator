0ace9b380f808222d486f004a5e5e7a6a300286d
MrAxeman777 <arsharma892@gmail.com>
2026-07-09 12:26:40 -0500

    Update awards.py

--- awards.py
+++ awards.py
@@ -1,35 +1,17 @@
-import random
+class Awards:
+
+    def __init__(self):
+
+        self.awards = []
 
 
-def check_awards(player):
+    def add_award(self, award):
 
-    print("\n🏅 Award Ceremony...")
+        if award not in self.awards:
 
-    # Young Player Award
-    if player.age <= 21 and player.rating >= 80:
-        award = "Young Player of the Year ⭐"
-        player.awards.append(award)
-        print(f"🏆 Won: {award}")
+            self.awards.append(award)
 
 
-    # Golden Boot
-    if player.goals >= 20:
-        award = "Golden Boot 🥾"
-        player.awards.append(award)
-        print(f"🏆 Won: {award}")
+    def get_awards(self):
 
-
-    # Player of the Year
-    if player.rating >= 90:
-        award = "Player of the Year 🌟"
-        player.awards.append(award)
-        print(f"🏆 Won: {award}")
-
-
-    # Ballon d'Or chance
-    ballon_chance = random.randint(1, 100)
-
-    if player.rating >= 92 and ballon_chance <= 20:
-        award = "Ballon d'Or 🥇"
-        player.awards.append(award)
-        print(f"🏆 Won: {award}")
+        return self.awards
